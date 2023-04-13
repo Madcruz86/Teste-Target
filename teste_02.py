caa5b37c57f0aff196b8c34e00c59ad9467b6358
Michel Alex Duarte Cruz <micheldcruz@gmail.com>
2023-04-13 13:54:37 -0300

    Update teste_02.py

--- teste_02.py
+++ teste_02.py
@@ -6,8 +6,12 @@ fib = [0, 1]
 while fib[-1] < num:
     fib.append(fib[-1] + fib[-2])
 
+# Mostar sequencia
+print(fib)
+
 # Verificar se o número informado pertence a sequencia
 if num in fib:
     print(f'Numero {num} pertence à sequência de Fibonacci!')
 else:
     print(f'Numero {num} não pertence à sequência de Fibonnaci.')
+
